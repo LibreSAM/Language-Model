185d40d6c24a590a445bc95fdef76e7686595dab
Robin Augenstein <augenstein.robin@student.dhbw-karlsruhe.de>
2023-04-19 13:25:16 +0200

    Count 1-gram

---
+++ learn/Onegram.cs
@@ -0,0 +1,15 @@
+﻿namespace learn;
+public class Onegram
+{
+    public string Word { get; }
+    public uint OccuranceCount { get; private set; }
+
+    public Onegram(string word) {
+        Word = word;
+        OccuranceCount = 0;
+    }
+
+    public void IncrementOccurenceCountByOne() {
+        OccuranceCount += 1;
+    }
+}
--- learn/Program.cs
+++ learn/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Bitte den Pfad zur Eingabedatei eingeben.");
+﻿using learn;
+
+Console.WriteLine("Bitte den Pfad zur Eingabedatei eingeben.");
 string? inputFilePath = Console.ReadLine();
 if (String.IsNullOrWhiteSpace(inputFilePath))
 {
@@ -15,4 +17,34 @@ catch (Exception ex)
 {
     Console.WriteLine($"Error while reading the input file: {ex.ToString()}");
     return;
+}
+
+var onegrams = new List<Onegram>();
+uint wordCount = 0;
+
+foreach (string line in inputLines)
+{
+    string[] words = line.Split(' ');
+
+    foreach (string word in words)
+    {
+        wordCount++;
+        var onegram = new Onegram(word);
+        Onegram? existing = onegrams.Find(o => o.Word == onegram.Word);
+        if (existing is not null)
+        {
+            existing.IncrementOccurenceCountByOne();
+        }
+        else
+        {
+            onegram.IncrementOccurenceCountByOne();
+            onegrams.Add(onegram);
+        }
+    }
+}
+
+Console.WriteLine("\\1-grams");
+foreach (Onegram item in onegrams)
+{
+    Console.WriteLine($"\"{item.Word}\" {item.OccuranceCount}");
 }
